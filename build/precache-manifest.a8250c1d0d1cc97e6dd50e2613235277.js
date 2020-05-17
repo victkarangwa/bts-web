dc21c3f9b0a50362d1cf1fb983844951effe9c55
mujohn26 <mujohn26@gmail.com>
2020-05-17 13:37:26 +0200

    feature(about):add about us page
    -  added about us page
    -  added responsiveness
    [finish #172876771]

---
+++ build/precache-manifest.a8250c1d0d1cc97e6dd50e2613235277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f274ac6386ceb8cb3f89d5e1726ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24a813cffa316b8cbf4d",
+    "url": "/static/js/2.ea9c1968.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea9c1968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c75541ac2384297e12",
+    "url": "/static/js/main.56e75f48.chunk.js"
+  },
+  {
+    "revision": "6dd7e3deec4091411108",
+    "url": "/static/js/runtime-main.47524fde.js"
+  }
+]);
---
+++ src/assets/styles/about.js
@@ -0,0 +1,14 @@
+import { height } from "@material-ui/system";
+import childrenPhoto from '../images/children.png';
+
+
+export const useStyles = theme => ({
+button:{
+    backgroundColor:'#D08A51',
+    color:'white',
+    '&:hover': {
+        background: "black",
+        border: '1px solid black',
+     },
+}
+});
